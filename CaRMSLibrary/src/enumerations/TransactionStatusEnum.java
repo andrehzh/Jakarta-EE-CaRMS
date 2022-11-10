1410ffbbb849e5a0d8f54de52b9aa73279541ec3
andrehzh <e0725806@u.nus.edu>
2022-11-11 02:27:14 +0800

    Merge origin/master Edited All entities to include relationship and data validation
    Conflicts:
    	CaRMS-ejb/src/conf/persistence.xml

---
+++ CaRMSLibrary/src/enumerations/TransactionStatusEnum.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enumerations;
+
+/**
+ *
+ * @author andre
+ */
+public enum TransactionStatusEnum {
+    PAID,
+    PAY_ON_SITE,
+    PENALTY_20,
+    PENALTY_50,
+    PENALTY_70;
+}
